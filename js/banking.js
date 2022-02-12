e3015fbe48851ceef6ab5fd689b1b3f67f8a6429
AriulIslamtonim <arifulislamtonim@gmail.com>
2022-02-12 22:33:39 +0600

    functional commit

--- js/banking.js
+++ js/banking.js
@@ -16,24 +16,30 @@ function updateTotalField (depositField, depositAmount) {
     depositTotal.innerText = previousDepositTotal + depositAmount;
 }
 
-function updateBalance (depositAmount, isAdd) {
+function getCurrentBalance () {
     const balance = document.getElementById('balance-total');
     const balanceTotal = balance.innerText;
     const previousBalance = parseFloat(balanceTotal);
-
+    return previousBalance;
+}
+function updateBalance (depositAmount, isAdd) {
+    const balance = document.getElementById('balance-total');
+   /*  const balanceTotal = balance.innerText;
+    const previousBalance = parseFloat(balanceTotal); */
+    const previousBalance = getCurrentBalance();
     if (isAdd == true){
         balance.innerText = previousBalance + depositAmount;
     }else {
         balance.innerText = previousBalance - depositAmount;
     }
-    
+   
 }
 
 document.getElementById("deposit-btn").addEventListener('click', function(){
     /* const depositInput = document.getElementById('deposit-input');
     const depositAmountText = depositInput.value;
     const depositAmount = parseFloat(depositAmountText); */
-    const depositAmount = getInputValue('deposit-input');
+   
     // get current deposit
    /*  const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
@@ -41,14 +47,21 @@ document.getElementById("deposit-btn").addEventListener('click', function(){
     const previousDepositTotal = parseFloat(depositTotalText);
 
     depositTotal.innerText = previousDepositTotal + depositAmount;
- */ updateTotalField ('deposit-total', depositAmount);
+
+ */ const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+    
+    updateTotalField ('deposit-total', depositAmount);
+    updateBalance (depositAmount, true);
+ }
+    
     /* depositInput.value = ''; */
 
    /*  const balance = document.getElementById('balance-total');
     const balanceTotal = balance.innerText;
     const previousBalance = parseFloat(balanceTotal);
     balance.innerText = previousBalance + depositAmount; */
-    updateBalance (depositAmount, true);
+   
 
 })
  document.getElementById('withdraw-btn').addEventListener('click', function() {
@@ -57,6 +70,14 @@ document.getElementById("deposit-btn").addEventListener('click', function(){
 
     const withdrawAmountTotal = parseFloat(withdrawAmount);
  */ const withdrawAmountTotal = getInputValue('withdraw-input');
+    const currentBalance = getCurrentBalance();
+     if (withdrawAmountTotal > 0 && withdrawAmountTotal <= currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmountTotal);
+        updateBalance (withdrawAmountTotal, false);
+     }
+     if(withdrawAmountTotal > currentBalance){
+        
+     }
 
     /* const withdrawTotal = document.getElementById('withdraw-total');
     const previousWithdraw = withdrawTotal.innerText;
@@ -64,12 +85,13 @@ document.getElementById("deposit-btn").addEventListener('click', function(){
     const previousWithdrawTotal = parseFloat(previousWithdraw);
 
     withdrawTotal.innerText = previousWithdrawTotal + withdrawAmountTotal; */
-    updateTotalField('withdraw-total', withdrawAmountTotal);
+    // updateTotalField('withdraw-total', withdrawAmountTotal);
     /* withdrawInput.value = ''; */
 
    /*  const balance = document.getElementById('balance-total');
     const mostBalance = balance.innerText;
     const previousBalance = parseFloat(mostBalance);
     balance.innerText = previousBalance - withdrawAmountTotal;
- */updateBalance (withdrawAmountTotal, false);
+ */
+// updateBalance (withdrawAmountTotal, false);
 })
